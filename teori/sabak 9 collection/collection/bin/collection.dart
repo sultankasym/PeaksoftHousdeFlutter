ed180533bbc86ba2c7b7883211776a3e440d1c1a
Sultan <Sultankasymov77@gamil.com>
2024-03-20 18:55:49 +0600

    collections

---
+++ teori/sabak 9 collection/collection/bin/collection.dart
@@ -0,0 +1,23 @@
+
+void main() {
+// int  a =1;
+// int b = 2;
+// int c = 3;
+//   print('$a, $b, $c');
+List num = [12,23,34, 56, 645 ,3 ,45,56];
+print(num[4]);// index alat
+List name= ['asan','aman','esen', 'sjas',3 ,45,56];
+print(name[3]);// index alat
+
+List aty= ['asan','aman','esen', 'sjas',3 ,45,56];
+final addName = name.add('bayzak');
+//name.add('galks');
+name.remove(1);
+print(aty);// index alat
+
+
+Set surname = {'asan','aman','esen', 3,'sjas',3 ,45,56}; // eki element kabyl albayt
+print(surname);
+Map mapName = {'1': 'asan', '2': 'esen', '3' : 'sa'};
+print(mapName['2']);
+}
